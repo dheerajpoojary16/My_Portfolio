e06632ea9afae3442f9ceb6fc76c5cf4ce39c497
deeraj <dheerajpoojarydheeraj@gmail.com>
2025-12-29 23:19:30 +0530

    29-12-2025
    UPDATED AND MODIFIED

---
+++ C#/first.cs
@@ -0,0 +1,12 @@
+using System;
+namespace alex
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            console.WriteLine("Hello!");
+            Console.WriteLine("alexandra");
+        }
+    }
+ }
